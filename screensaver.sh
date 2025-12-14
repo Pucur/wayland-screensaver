8be4892b5bd31a097a53339ad10615f5488969be
Pucur <62958352+Pucur@users.noreply.github.com>
2025-12-14 17:27:49 +0100

    Update screensaver.sh to v1.3

--- screensaver.sh
+++ screensaver.sh
@@ -1,8 +1,36 @@
 #!/usr/bin/env bash
 
+ _       _                _                      _
+# ( )  _  ( )              (_ )                   ( )
+# | | ( ) | |   _ _  _   _  | |    _ _   ___     _| |     ___    ___  _ __   __     __    ___    ___    _ _  _   _    __   _ __
+# | | | | | | /'_` )( ) ( ) | |  /'_` )/' _ `\ /'_` |   /',__) /'___)( '__)/'__`\ /'__`\/' _ `\/',__) /'_` )( ) ( ) /'__`\( '__)
+# | (_/ \_) |( (_| || (_) | | | ( (_| || ( ) |( (_| |   \__, \( (___ | |  (  ___/(  ___/| ( ) |\__, \( (_| || \_/ |(  ___/| |
+# `\___x___/'`\__,_)`\__, |(___)`\__,_)(_) (_)`\__,_)   (____/`\____)(_)  `\____)`\____)(_) (_)(____/`\__,_)`\___/'`\____)(_)
+#                   ( )_| |
+#                   `\___/'
+
+# ==============================
+# VARIABLES
+# ==============================
+
+inhibit=false
+last_activity_state="unknown"
+app=tixati # Put your custom app name here or leave it empty
+
+echo "༄˖°.🍃.ೃ࿔*:･ Swayidle Inhibit Watcher v1.3 ~~ Created by Pucur - 2025.12.14 ~~ https://github.com/Pucur/wayland-screensaver ༄˖°.🍃.ೃ࿔*:･"
+
+# Timestamp
+stamp() {
+    echo "[$(date '+%H:%M:%S')] $*"
+}
+
+# ==============================
+# Start XscreenSaver
+# ==============================
+
 while true; do
     if pgrep -x xscreensaver > /dev/null; then
-        echo "✅ XScreenSaver is already running. Exiting wait loop."
+        stamp "✅ XScreenSaver is already running. Exiting wait loop."
         break
     fi
 
@@ -13,30 +41,25 @@ while true; do
     sleep 1
 
     if pgrep -x xscreensaver > /dev/null; then
-        echo "✅ XScreenSaver started successfully."
+        stamp "✅ XScreenSaver started successfully."
         break
     fi
 
-    echo "❌ Failed to start xscreensaver. Retrying in 1 second..."
+    stamp "❌ Failed to start xscreensaver. Retrying in 1 second ..."
     sleep 1
 done
 
 # Kill existing swayidle and start it a fresh one
 pkill swayidle 2>/dev/null
-swayidle -w timeout 120 'xscreensaver-command -activate' timeout 900 'systemctl suspend' &
-
-# ==============================
-# VARIABLES
-# ==============================
-youtube_or_other_inhibit=false
-last_activity_state="unknown"
-POLL_INTERVAL=5
+swayidle -w timeout 120 'xscreensaver-command -activate' timeout 900 'systemctl suspend' 2>/dev/null &
 
 # ==============================
 # FUNCTIONS
 # ==============================
+
+# Detecting custom app or game mode is running or not
 is_activity_running() {
-    if pgrep -x "tixati" > /dev/null; then
+    if pgrep -x "$app" > /dev/null; then
         return 0
     fi
     if ! gamemoded -s 2>/dev/null | grep -q "inactive"; then
@@ -45,6 +68,7 @@ is_activity_running() {
     return 1
 }
 
+# Determine which screensaver mod to start
 start_swayidle() {
     pkill swayidle 2>/dev/null
 
@@ -53,10 +77,18 @@ start_swayidle() {
     fi
 
     if is_activity_running; then
+
+        # When only game or app running
         pkill swayidle 2>/dev/null
-        swayidle -w timeout 120 'xscreensaver-command -activate' &
+        stamp "🎮 No video inhibit, starting Game mode screensaver ..."
+        swayidle -w timeout 120 'xscreensaver-command -activate' 2>/dev/null &
+        pkill -f "xscreensaver-gfx"
     else
-        swayidle -w timeout 120 'xscreensaver-command -activate' timeout 900 'systemctl suspend' &
+        # When no process is running
+
+        stamp "🏞️ No video inhibit, no game running, starting screensaver ..."
+        swayidle -w timeout 120 'xscreensaver-command -activate' timeout 900 'systemctl suspend' 2>/dev/null &
+        pkill -f "xscreensaver-gfx"
     fi
 }
 
@@ -66,27 +98,29 @@ start_swayidle() {
 dbus-monitor --session "interface='org.freedesktop.ScreenSaver'" |
 while read -r line; do
 
+    # When only game or app running
     if [[ "$line" =~ member=Inhibit ]]; then
         read -r nextline
 
         if [[ "$nextline" =~ "com.feralinteractive.GameMode" ]]; then
             start_swayidle
         else
-            youtube_or_other_inhibit=true
+        stamp "📺 Video playing, turning screensaver off ..."
+            inhibit=true
             pkill swayidle
+            pkill -f "xscreensaver-gfx"
         fi
 
+    # When no process is running
     elif [[ "$line" =~ member=UnInhibit ]]; then
         read -r nextline
-
         if ! [[ "$nextline" =~ "com.feralinteractive.GameMode" ]]; then
-            youtube_or_other_inhibit=false
-            start_swayidle
+            inhibit=false
+                start_swayidle
         fi
     fi
 
-    # Poll Tixati/GameMode activity
-    if [ $((SECONDS % POLL_INTERVAL)) -eq 0 ]; then
+    # Check state change
         if is_activity_running; then
             current_state="running"
         else
@@ -94,9 +128,8 @@ while read -r line; do
         fi
 
         if [ "$current_state" != "$last_activity_state" ]; then
-            echo "Activity state changed → $current_state"
+            stamp "🚩 Activity state changed → $current_state"
             start_swayidle
             last_activity_state="$current_state"
         fi
-    fi
 done
